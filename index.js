3cd1403f5f49bf6cf9b4c87b184a1fd2971bd6ab
Carlos Didier Fernandez <didierfdz@gmail.com>
2019-06-07 17:33:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue() {
+function updateObjectWithKeyAndValue(object, key, value) {
   recipes[key] = value;
   return recipes;
 }
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, 'prop2', value) {
   var newObj = Object.assign({}, recipes);
   newObj[key] = value;
   return newObj;
